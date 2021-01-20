8719ea2461ce40655fc0785b3950694452edba04
yangjiajian <287023075@qq.com>
2021-01-20 16:27:44 +0800

    增加eslint，数据库配置

---
+++ src/db/seq.js
@@ -0,0 +1,22 @@
+const Seqelize = require('sequelize')
+const { MYSQL_CONF } = require('../conf/db')
+const { isProd } = require('../utils/env')
+
+const { host, user, password, database } = MYSQL_CONF
+
+const conf = {
+    host,
+    dialect: 'mysql'
+}
+
+if(isProd) { // 生产环境设置连接池
+    conf.pool = {
+        max: 5,
+        min: 0,
+        idle: 10000, //连接池10s没有使用则释放
+    }
+}
+
+const seq = new Seqelize(database, user, password, conf)
+
+module.exports = seq
